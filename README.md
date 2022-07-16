9c76cf6575a55e4061d2f679aade8b6c191fc5cb
juliakhalina <juli79a4@edu.easj.dk>
2022-07-16 23:00:34 +0200

    activeSlide-by-js

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. Create html and css files with styles
 2. Create app.js with code for animation of slides
 3. Images from http://unsplash.com
+4. Create js script for active card: the active card as a item of array
+5. Create js function slidesPlugin for the active element
--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const slides = document.querySelectorAll('.slide')
+// create active card as the element of array
+//slides[3].classList.add('active')
+
+// create function for active card
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll('.slide')
+
+    //put value activeSlide in the array
+    slides[activeSlide].classList.add('active')
 
 for (const slide of slides) {
      slide.addEventListener('click', () => {
@@ -12,3 +20,9 @@ for (const slide of slides) {
         slide.classList.remove('active')
       })
   }
+}
+
+//call to function
+// if didn't send parameters in the function, so activeSlide=0 or first card is active
+slidesPlugin(2)
+ 
